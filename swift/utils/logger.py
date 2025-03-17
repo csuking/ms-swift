4c9846ffc2fd15c1605067bf87efb06ea041b756
yaya <2529375619@qq.com>
2025-03-17 10:31:43 +0800

    feat: support colorful log

--- swift/utils/logger.py
+++ swift/utils/logger.py
@@ -40,59 +40,73 @@ def warning_once(self, msg, *args, **kwargs):
 
 
 def get_logger(log_file: Optional[str] = None, log_level: Optional[int] = None, file_mode: str = 'w'):
-    """ Get logging logger
+    import colorlog
+    """Get logging logger with colored output
 
     Args:
-        log_file: Log filename, if specified, file handler will be added to
-            logger
+        log_file: Log pathname, if specified, file handler will be added to logger
         log_level: Logging level.
-        file_mode: Specifies the mode to open the file, if filename is
-            specified (if filemode is unspecified, it defaults to 'w').
+        file_mode: Specifies the mode to open the file
     """
     if log_level is None:
-        log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
-        log_level = getattr(logging, log_level, logging.INFO)
+        log_level = os.getenv('LOG_LEVEL', 'DEBUG').upper()  # 默认DEBUG级别
+        log_level = getattr(logging, log_level, logging.DEBUG)  # 允许控制台控制级别
+    
     logger_name = __name__.split('.')[0]
     logger = logging.getLogger(logger_name)
     logger.propagate = False
-    if logger_name in init_loggers:
-        add_file_handler_if_needed(logger, log_file, file_mode, log_level)
-        return logger
 
-    # handle duplicate logs to the console
-    # Starting in 1.8.0, PyTorch DDP attaches a StreamHandler <stderr> (NOTSET)
-    # to the root logger. As logger.propagate is True by default, this root
-    # level handler causes logging messages from rank>0 processes to
-    # unexpectedly show up on the console, creating much unwanted clutter.
-    # To fix this issue, we set the root logger's StreamHandler, if any, to log
-    # at the ERROR level.
-    for handler in logger.root.handlers:
-        if type(handler) is logging.StreamHandler:
-            handler.setLevel(logging.ERROR)
+    # 清除现有的handlers，避免重复
+    if logger.handlers:
+        logger.handlers.clear()
 
+    # 创建彩色控制台处理器
     stream_handler = logging.StreamHandler()
-    handlers = [stream_handler]
+    stream_handler.setLevel(logging.DEBUG)  # 显式允许 DEBUG 级别
 
-    is_worker0 = _is_local_master()
+    color_formatter = colorlog.ColoredFormatter(
+        "%(log_color)s[%(pathname)s:%(lineno)d] %(message)s%(reset)s",  
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        reset=True,
+        style='%'
+    )
+    stream_handler.setFormatter(color_formatter)
+    logger.addHandler(stream_handler)
 
-    if is_worker0 and log_file is not None:
+    # 如果需要文件日志
+    if log_file is not None:
         file_handler = logging.FileHandler(log_file, file_mode)
-        handlers.append(file_handler)
+        file_handler.setLevel(logging.DEBUG)  # 允许所有日志级别写入文件
+        file_handler.setFormatter(logging.Formatter('[%(levelname)s:%(pathname)s:%(lineno)d] %(message)s'))
+        logger.addHandler(file_handler)
 
-    for handler in handlers:
-        handler.setFormatter(logger_format)
-        handler.setLevel(log_level)
-        logger.addHandler(handler)
+    # 确保 logger 级别不会限制 DEBUG
+    logger.setLevel(logging.DEBUG)
 
-    if is_worker0:
-        logger.setLevel(log_level)
-    else:
-        logger.setLevel(logging.ERROR)
+    # 添加一次性打印方法
+    def info_once(self, msg, *args, **kwargs):
+        if not hasattr(self, "_info_logged"):
+            self._info_logged = set()
+        if msg not in self._info_logged:
+            self._info_logged.add(msg)
+            self.info(msg, *args, **kwargs)
 
-    init_loggers[logger_name] = True
+    def warning_once(self, msg, *args, **kwargs):
+        if not hasattr(self, "_warning_logged"):
+            self._warning_logged = set()
+        if msg not in self._warning_logged:
+            self._warning_logged.add(msg)
+            self.warning(msg, *args, **kwargs)
 
     logger.info_once = MethodType(info_once, logger)
     logger.warning_once = MethodType(warning_once, logger)
+
     return logger
 
 
